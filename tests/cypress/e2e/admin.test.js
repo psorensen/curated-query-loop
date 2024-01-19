d3ba89dca77d12fb66d75336b1aec259dc97f598
Peter Sorensen <peter.sorensen@get10up.com>
2024-01-18 19:12:01 -0800

    adds first test

--- tests/cypress/e2e/admin.test.js
+++ tests/cypress/e2e/admin.test.js
@@ -1,15 +1,43 @@
-describe('Admin can login and open dashboard', () => {
+import { getIframe } from "../functions/getIframe";
+const BLOCKTYPE = 'core/query';
+describe("Admin can login and open dashboard", () => {
 	beforeEach(() => {
 		cy.login();
 	});
 
-	it('Open dashboard', () => {
+	it("Open dashboard", () => {
 		cy.visit(`/wp-admin`);
-		cy.get('h1').should('contain', 'Dashboard');
+		cy.get("h1").should("contain", "Dashboard");
 	});
 
-	it('Activate Hello Dolly and deactivate it back', () => {
-		cy.activatePlugin('hello-dolly');
-		cy.deactivatePlugin('hello-dolly');
+	it("Activate Curated Query Loop and deactivate it back", () => {
+		cy.deactivatePlugin("query-loop-post-picker");
+		cy.activatePlugin("query-loop-post-picker");
+	});
+
+	it("Create new post, add Curated Query Loop and add a post", () => {
+		cy.createPost({});
+		cy.get('.edit-post-header-toolbar__inserter-toggle').click();
+		cy.contains('Curated Query Loop').click();
+		cy.get('.edit-post-header-toolbar__inserter-toggle').click();
+		// Assert that the block variation is inserted successfully and has approporiate class
+		cy.get('body').then(() => {
+			const iframe = getIframe('iframe[name="editor-canvas"]')
+			iframe.find(`.wp-block[data-type="${BLOCKTYPE}"].has-no-posts`)
+		});
+
+		// Curate a post
+		cy.openDocumentSettingsSidebar('Block')
+		cy.get('.tenup-content-picker input').type('Hello World')
+		cy.get('.block-editor-link-control__search-item').first().click()
+
+		cy.get('body').then($body => {
+			const iframe = getIframe('iframe[name="editor-canvas"]')
+			iframe
+				.find(`.wp-block[data-type="${BLOCKTYPE}"]`)
+				.should('not.have.class', 'has-no-posts')
+				.find('h2')
+				.should('contain', 'Hello world!')
+		});
 	});
 });
--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+// Cypress.Commands.add('closePatternModal', () => {
+//     cy.get('.modal-header > .close').click()
+// });
