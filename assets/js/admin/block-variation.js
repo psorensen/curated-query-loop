36e366869831f7339e9574228d26dd9289ef4e19
Peter Sorensen <peter.sorensen@get10up.com>
2024-08-30 07:11:35 -0700

    fix icons

--- assets/js/admin/block-variation.js
+++ assets/js/admin/block-variation.js
@@ -1,11 +1,13 @@
 import { registerBlockVariation } from '@wordpress/blocks';
 import { __ } from '@wordpress/i18n';
+import { postList as icon } from '@wordpress/icons';
 import { NAMESPACE } from './namespace';
 
 registerBlockVariation('core/query', {
 	name: NAMESPACE,
 	title: 'Curated Query Loop',
 	isActive: ['namespace'],
+	icon,
 	description: __(
 		'A variation of the Query Loop that allows for curating specific posts',
 		'curated-query-loop',
